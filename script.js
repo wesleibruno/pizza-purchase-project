72dae57657fb1e87b98f9f21c3fff0b846d3371e
Weslei Bruno <weslei945@gmail.com>
2020-10-17 18:08:30 -0300

    clicar em selecionar tamanhos

--- script.js
+++ script.js
@@ -57,8 +57,16 @@ c('.pizzaInfo--qtmenos').addEventListener('click', ()=> {
     if(modalQt > 1)
     modalQt--;
     c('.pizzaInfo--qt').innerHTML = modalQt;
-})
+});
 c('.pizzaInfo--qtmais').addEventListener('click', ()=> {
     modalQt++;
     c('.pizzaInfo--qt').innerHTML = modalQt;
-})
+});
+
+cs('.pizzaInfo--size').forEach((size, sizeIndex)=>{
+    size.addEventListener('click', (e)=> {
+        c('.pizzaInfo--size.selected').classList.remove('selected');
+        size.classList.add('selected');   
+    })
+
+});
